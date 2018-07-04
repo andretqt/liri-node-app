1154c38def171e85416199edc524cf6f73919762
yung dev <andre.torquati@gmail.com>
2018-07-03 20:01:09 -0400

    finished myTweets()

--- liri.js
+++ liri.js
@@ -1,39 +1,43 @@
 require("dotenv").config();
+var moment = require('moment')
 var request = require('request');
-var Twitter = require('twitter');
 // var Spotify = require('node-spotify-api');
 var keys = require('./keys.js');
 var fs = require("fs");
 // var spotify = new Spotify(keys.spotify);
-
 //--------------------
 var command = process.argv[2];
 
-
-var myTweets = function() {
-    var client = new Twitter({
-        consumer_key: keys.consumer_key,
-		consumer_secret: keys.consumer_secret,
-		access_token_key: keys.access_token_key,
-		access_token_secret: keys.access_token_secret
-    });
-  
-    client.get('https://api.twitter.com/1.1/lists/statuses.json?slug=teams&owner_screen_name=andretqt&count=1', function(error, tweets, response) {
-        console.log(response);
+//defining our functions
+var myTweets = function () {
+    let tweetArray = [];
+    var Twitter = require('twitter');
+    var client = new Twitter(keys.twitter);
+    var params = {
+		screen_name: 'andretqt',
+		count: 20
+    };
+    var i = params.count;
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
 		if(error) { // if there IS an error
-			console.log('Error occurred: ' + error);
-		} else { // if there is NO error
-	  	console.log("My 20 Most Recent Tweets");
-	  	console.log("");
-	  	for(var i = 0; i < tweets.length; i++) {
-	  		console.log("( #" + (i + 1) + " )  " + tweets[i].text);
-	  		console.log("Created:  " + tweets[i].created_at);
-	  		console.log("");
+			console.log('Error occured: ' + error);
+        } else { // if there is NO error
+            console.log(`My last ${params.count} tweets:`)
+            tweets.forEach(tweetElement => {
+                var tweetDate = tweetElement.created_at;
+                var newDate = moment(tweetDate, 'dd MMM DD HH:mm:ss ZZ YYYY', 'en');
+                tweetArray.push([tweetElement.text, newDate]);
+                console.log(`#${i}. '${tweetElement.text}' tweeted on ${newDate}`);
+                i--;
+            });
 	  	}
 	  }
-	});
-};
+	);
+}
+
+// App functionality due to user input
 if(command === "my-tweets") {
-    myTweets();
+	myTweets();
+} else { // use case where command is given but not recognized
+	console.log("Command not recognized! Please try again.")
 };
-console.log(keys.twitter);
